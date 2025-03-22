01227626fa0d711eca605e73e0626678b30a553d
Vedran Hrncic <vedran.hrncic@gmail.com>
2025-03-22 12:24:19 +0100

    Add docs templates

--- lib/rails_template/main.rb
+++ lib/rails_template/main.rb
@@ -5,3 +5,6 @@ require_template('util')
 template_options.uses_node_runtime = yes?('Will this application need Node runtime? [No]', :green)
 
 template 'README.md.tt', force: true
+copy_file 'docs/README.md'
+copy_file 'docs/architecture.md'
+copy_file 'docs/development_workflow.md'
