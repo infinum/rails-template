9d2209440e65c80a63ba58bf31d5ace38fd510c3
Vedran Hrncic <vedran.hrncic@gmail.com>
2025-03-22 12:51:40 +0100

    Add bin/update template

--- lib/rails_template/main.rb
+++ lib/rails_template/main.rb
@@ -13,3 +13,5 @@ create_file 'config/environments/staging.rb', "require_relative 'production'"
 template 'config/database.yml.tt', force: true
 
 template 'bin/setup', force: true
+template 'bin/update.tt'
+chmod 'bin/update', 0o755, verbose: false
