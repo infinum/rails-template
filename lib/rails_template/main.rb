a2452f3233da388e48c62d6a3d6fabf8bfb7acb8
Vedran Hrncic <vedran.hrncic@gmail.com>
2025-03-22 15:36:54 +0100

    Add Bugsnag initializer

--- lib/rails_template/main.rb
+++ lib/rails_template/main.rb
@@ -16,9 +16,11 @@ template 'config/database.yml.tt', force: true
 
 template 'sample.env.tt'
 template 'bin/setup.tt', force: true
+chmod 'bin/setup', 0o755, verbose: false
 template 'bin/update.tt'
 chmod 'bin/update', 0o755, verbose: false
 copy_file 'bin/ci-checks'
 chmod 'bin/ci-checks', 0o755, verbose: false
 
 copy_file '.bundle/ci/config'
+copy_file 'config/initializers/bugsnag.rb'
