5d0a60c6d7adb4e2d2065ff5c5ec5f6c510d5a9b
anamarijabalaban <anamarija.balaban@gmail.com>
2025-06-18 09:39:22 +0200

    Add license_finder recipe and run license_finder commands in run_generators.rb

---
+++ recipes/license_finder.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+append_to_file 'Gemfile', after: /gem 'rubocop-infinum'.*\n/ do
+  <<~HEREDOC
+    gem 'license_finder', require: false
+  HEREDOC
+end
+
+append_to_file '.overcommit.yml', after: /command: \['bundle', 'exec', 'rubocop'\].*\n/ do
+  <<~HEREDOC
+
+    LicenseFinder:
+      enabled: true
+      on_warn: fail
+      command: ['bundle', 'exec', 'license_finder']
+      requires_files: true
+      include:
+        - 'Gemfile*'
+        - 'package.json'
+
+  HEREDOC
+end
