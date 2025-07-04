d4a9cb63eb3e76724e6a8ff7b736c0c73e926542
anamarijabalaban <anamarija.balaban@gmail.com>
2025-07-04 08:57:28 +0200

    Check if development and test group exists in Gemfile generation

--- recipes/gemfile.rb
+++ recipes/gemfile.rb
@@ -13,7 +13,8 @@ append_to_file 'Gemfile', after: /gem "rails".*\n/ do
     gem 'strong_migrations'
   HEREDOC
 end
-append_to_file 'Gemfile', after: /group :development do\n/ do
+
+def development_group_gems
   <<~HEREDOC
     gem 'annotate'
     gem 'better_errors'
@@ -23,11 +24,39 @@ append_to_file 'Gemfile', after: /group :development do\n/ do
     gem 'thor', require: false
   HEREDOC
 end
-append_to_file 'Gemfile', after: /group :test do\n/ do
-  <<~HEREDOC
-    gem 'rspec-rails'
-  HEREDOC
+
+if File.foreach('Gemfile').grep(/group :development do/).any?
+  append_to_file 'Gemfile', after: /group :development do\n/ do
+    development_group_gems
+  end
+else
+  append_to_file 'Gemfile' do
+    <<~HEREDOC
+
+      group :development do
+        #{development_group_gems}
+      end
+    HEREDOC
+  end
 end
+
+if File.foreach('Gemfile').grep(/group :test do/).any?
+  append_to_file 'Gemfile', after: /group :test do\n/ do
+    <<~HEREDOC
+      gem 'rspec-rails'
+    HEREDOC
+  end
+else
+  append_to_file 'Gemfile' do
+    <<~HEREDOC
+
+      group :test do
+        gem 'rspec-rails'
+      end
+    HEREDOC
+  end
+end
+
 append_to_file 'Gemfile' do
   <<~HEREDOC
 
